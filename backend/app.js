409812f7282fb357059c5122ce366c6cb13a8530
bodlagayathri <84134684+bodlagayathri@users.noreply.github.com>
2021-05-14 11:19:38 -0500

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+mongoose.connect(
+    "mongodb+srv://Gayathri:ke7bnSZoh8yNgpn0@cluster0.lh8w1.mongodb.net/node-angular?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        
+    }
+  )//ke7bnSZoh8yNgpn0
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+  });
+  
+  app.use("/api/posts", postsRoutes);
+  app.use("/api/user", userRoutes);
+
+  module.exports = app;
+  
